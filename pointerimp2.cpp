56eb34da27d062f08b916afc17e72983db8397e7
suryapratap1121 <thakur.surya1303@gmail.com>
2025-08-28 14:41:02 +0530

    Create pointerimp2.cpp

---
+++ pointerimp2.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+void swap(int *x, int *y){
+    int aa;
+    aa=*x;
+    *x=*y;
+    *y=aa;
+cout << "swapped value of a "<< *x<<endl;
+cout <<"swapped value of b "<<  *y<< endl;
+}
+int main(){
+ int a=3,b=5;
+ swap(&a,&b);
+ cout << a<<endl;
+ cout<< b;
+}
